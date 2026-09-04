474acb868969aa4a5d8ece3b7895ba93bdb694f8
HB <toouhu14@gmail.com>
2026-09-04 23:44:17 +0900

    Roomの編集・更新・削除機能を追加

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -6,7 +6,7 @@ class RoomsController < ApplicationController
   end
 
   def show
-  @room = Room.find(params[:id])
+    @room = Room.find(params[:id])
   end
 
   def new
@@ -24,6 +24,24 @@ class RoomsController < ApplicationController
   end
 
   def edit
+    @room = current_user.rooms.find(params[:id])
+  end
+
+  def update
+    @room = current_user.rooms.find(params[:id])
+
+    if @room.update(room_params)
+      redirect_to @room, notice: "施設を更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @room = current_user.rooms.find(params[:id])
+    @room.destroy
+
+    redirect_to rooms_path, notice: "施設を削除しました"
   end
 
   private
